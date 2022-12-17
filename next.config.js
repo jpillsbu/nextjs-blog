56be00a916212ca008279416fe6f33cf58b9c357
John Pillsbury <jpillsbu@gmail.com>
2022-12-17 16:08:07 -0500

    Completed application

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ["links.papareact.com", "cdn.sanity.io"],
+  },
+  output: "standalone",
+};
